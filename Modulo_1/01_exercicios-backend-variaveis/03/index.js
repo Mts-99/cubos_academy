ee98cd61d399f86160ffb12aab4235276933c857
Matheus Marciano de Oliveira <matheusmarciano294@gmail.com>
2023-08-19 13:25:07 -0300

    03/index.js

---
+++ Modulo_1/01_exercicios-backend-variaveis/03/index.js
@@ -0,0 +1,6 @@
+let valorDoTenis = 129.99;
+let dinheiroEmMaos = 80;
+let resultadoC = (valorDoTenis - dinheiroEmMaos);
+let resultadoD = (resultadoC * 100);
+
+console.log(resultadoD / valorDoTenis +'%');
