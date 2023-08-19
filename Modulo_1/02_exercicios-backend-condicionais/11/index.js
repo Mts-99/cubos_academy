a5bd3a929a16a247bfe70e3681cdc9f9102a2a56
Matheus Marciano de Oliveira <matheusmarciano294@gmail.com>
2023-08-19 20:01:59 -0300

    Valor da mensalidade

---
+++ Modulo_1/02_exercicios-backend-condicionais/11/index.js
@@ -0,0 +1,12 @@
+//renda mensal do aluno, em centavos.
+const rendaMensalEmCentavos = 300000;
+
+// Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
+const mesesDecorridos = 12;
+
+// Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
+const totalJaPagoPeloAluno = 1000000;
+const porcentagem = 0.18;
+
+const parcela = rendaMensalEmCentavos * porcentagem;
+console.log (`O valor da parcela desse mês é R$ ${parcela/100} reais`);
