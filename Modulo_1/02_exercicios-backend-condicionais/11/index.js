127484317451ff293c9a75fb9f5cafff103bb700
Matheus Marciano de Oliveira <matheusmarciano294@gmail.com>
2023-08-19 21:17:45 -0300

    Valor da parcela do Sucesso Compartilhado.

--- Modulo_1/02_exercicios-backend-condicionais/11/index.js
+++ Modulo_1/02_exercicios-backend-condicionais/11/index.js
@@ -1,12 +1,21 @@
 //renda mensal do aluno, em centavos.
-const rendaMensalEmCentavos = 300000;
+const rendaMensalEmCentavos = 1_500_00;
+const porcentagem = 0.18;                                
+const parcela = rendaMensalEmCentavos * porcentagem;    
+const mesesDecorridos = 6;
+const totalJaPagoPeloAluno = 17_000_00;                   
+const valorApagar = 18_000_00
+const falta = valorApagar - totalJaPagoPeloAluno;
 
-// Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
-const mesesDecorridos = 12;
-
-// Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
-const totalJaPagoPeloAluno = 1000000;
-const porcentagem = 0.18;
-
-const parcela = rendaMensalEmCentavos * porcentagem;
-console.log (`O valor da parcela desse mês é R$ ${parcela/100} reais`);
+if (valorApagar === totalJaPagoPeloAluno){
+    console.log('Divida paga!')
+}
+else if (rendaMensalEmCentavos /100 >= 2_000 && mesesDecorridos <= 60){
+    console.log(`Valor da parcela será R$ ${parcela / 100} reais.`)
+}
+else if (mesesDecorridos >= 60){
+    console.log('O periodo de quitação da divida foi encerrado pois passou dos 60 meses.')
+} 
+else if (rendaMensalEmCentavos /100 <= 2_000){
+    console.log('O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois a renda do estudante está abaixo do valor mínimo de R$ 2.000 reais.')
+}
