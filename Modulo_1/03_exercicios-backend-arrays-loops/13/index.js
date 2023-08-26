d318cf747e61022c1325459e73b64a1d45d9b6e5
Matheus Marciano de Oliveira <matheusmarciano294@gmail.com>
2023-08-26 12:12:47 -0300

    Verificando disjuntore.

---
+++ Modulo_1/03_exercicios-backend-arrays-loops/13/index.js
@@ -0,0 +1,7 @@
+const disjuntores = [false, false, true, false, false, true, false, false];
+
+for (let numDidJuntor = 0; numDidJuntor < disjuntores.length; numDidJuntor++) {
+    if(disjuntores[numDidJuntor]){
+        console.log(numDidJuntor);
+    }
+}
