6d60cd7d9dd93555d74d2dad3001a5f8ba614268
Matheus Marciano de Oliveira <matheusmarciano294@gmail.com>
2023-08-26 10:53:30 -0300

    Lista de veículos.

---
+++ Modulo_1/04_exercicios-backend-objetos/02/index.js
@@ -0,0 +1,26 @@
+const carro_1 ={
+    marca: 'Volkswagen',
+    modelo: 'Gol',
+    ano: 2023,
+    cor: 'Preto',
+    quantidade_portas: 4,
+    automatico: true
+};
+const carro_2 ={
+    marca: 'Fiat',
+    modelo: 'Uno',
+    ano: 2014,
+    cor: 'Azul',
+    quantidade_portas: 4,
+    automatico: false
+};
+const carro_3 ={
+    marca: 'Ford',
+    modelo: 'Focus',
+    ano: 2013,
+    cor: 'Prata',
+    quantidade_portas: 4,
+    automatico: false
+};
+
+console.log(carro_3.automatico);
