bf375832b32852ddaf35823ec78406c3441bac85
Matheus Marciano de Oliveira <matheusmarciano294@gmail.com>
2023-08-26 10:33:30 -0300

    Cadastro de usuário.

---
+++ Modulo_1/04_exercicios-backend-objetos/01/index.js
@@ -0,0 +1,19 @@
+const endereco = {
+    rua: 'avenida brasil',
+    numero: 999,
+    complemento:'Rua sem saída',
+    bairro: 'Brasília',
+    cep: 29999990,
+    cidade: 'Rio de janeiro',
+    estado: 'RJ',
+    pais: 'Brasil'
+
+};
+const usuario = {
+    nome: 'Matheus',
+    email: 'matheus@gmail.com',
+    telefone: 21965167798,
+    data_nascimento: '11/10/1999',
+    endereco
+};
+console.log(usuario);
