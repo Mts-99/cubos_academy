c831a304914a5afe9b9b19a9be76ff874820283c
Matheus Marciano de Oliveira <matheusmarciano294@gmail.com>
2023-09-01 17:04:36 -0300

    URL amigável.

---
+++ Modulo_1/06_exercicios-backend-metodos-strings/03/index.js
@@ -0,0 +1,17 @@
+const texto = "Aprenda programar do zero na Cubos Academy";
+
+const trasformarTextoEmUrl = (texto) => {
+    let url = "";
+
+    const arrayPalavras = texto.split(' ');
+    for (let i = 0; i < arrayPalavras.length; i++) {
+        if (i > 0) {
+        url += `-${arrayPalavras[i]}`; 
+        } else {
+        url += `${arrayPalavras[i]}`; 
+        }
+    }
+    console.log(url.toLocaleLowerCase());
+}
+
+trasformarTextoEmUrl(texto);
