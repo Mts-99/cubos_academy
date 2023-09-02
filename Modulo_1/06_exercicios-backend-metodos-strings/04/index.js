1167637c68e284aceef01538ff5c00cf7a6e227e
Matheus Marciano de Oliveira <matheusmarciano294@gmail.com>
2023-09-02 10:58:40 -0300

    Sistema de formatação de dados do formulário.

---
+++ Modulo_1/06_exercicios-backend-metodos-strings/04/index.js
@@ -0,0 +1,17 @@
+let identificador = "123";
+let nome = "José silva costa";
+let email = "      jose@email.com  ";
+
+identificador = identificador.padStart(6, '0');
+
+let nomeMaiusculo = "";
+
+for (const item of nome.split(' ')){
+    nomeMaiusculo += `${item.slice(0, 1).toLocaleUpperCase()}${item.slice(1).toLocaleLowerCase()} `
+}
+nome = nomeMaiusculo.trim();
+email = email.trim();
+
+console.log(identificador);
+console.log(nome);
+console.log(email);
