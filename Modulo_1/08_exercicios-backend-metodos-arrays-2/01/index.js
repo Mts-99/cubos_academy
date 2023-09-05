1e6036486a741438b8f9edb8a8e14e15dc540a99
Matheus Marciano de Oliveira <matheusmarciano294@gmail.com>
2023-09-05 15:13:22 -0300

    Sistema para biblioteca.

---
+++ Modulo_1/08_exercicios-backend-metodos-arrays-2/01/index.js
@@ -0,0 +1,6 @@
+const livros = ['Guerra e Paz', 'A Montanha Mágica', 'Cem Anos de Solidão', 'Dom Quixote', 'A Divina Comédia'];
+const nomeDoLivro = "A Montanha Mágica";
+
+const livroEncontrado = livros.indexOf(nomeDoLivro) + 1;
+
+console.log(`O livro está na posição ${livroEncontrado}.`);
