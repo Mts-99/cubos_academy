11982a17b8f2844bbac6288b9c841c0fa8e714e1
Matheus Marciano de Oliveira <matheusmarciano294@gmail.com>
2023-09-06 09:05:13 -0300

    Filtro e validação de usuários.

---
+++ Modulo_1/08_exercicios-backend-metodos-arrays-2/05/index.js
@@ -0,0 +1,45 @@
+const usuarios = [
+    {
+      nome: "André",
+      idade: 29,
+      habilitado: false,
+    },
+    {
+      nome: "Marcos",
+      idade: 70,
+      habilitado: true,
+    },
+    {
+      nome: "Ana",
+      idade: 35,
+      habilitado: true,
+    },
+    {
+      nome: "Vinícius",
+      idade: 44,
+      habilitado: true,
+    },
+    {
+      nome: "Carlos",
+      idade: 17,
+      habilitado: false,
+    },
+    {
+      nome: "Maria",
+      idade: 19,
+      habilitado: true,
+    },
+  ];
+  const usuariosFaixaEtaria = usuarios.filter(function(usuario) {
+    return usuario.idade >= 18 && usuario.idade <= 65;   
+  });
+
+  const todosHabilitados = usuariosFaixaEtaria.every((usuario) => {
+    return usuario.habilitado === true;
+  });
+
+  if (todosHabilitados) {
+    console.log('todos passaram no teste');
+  } else {
+    console.log('todos precisam estar habilitados');
+  }
