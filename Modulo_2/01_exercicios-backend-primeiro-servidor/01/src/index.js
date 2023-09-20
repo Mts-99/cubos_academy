f8e9764ba5c710bf24c23baf5dd7975d30a1f35d
Matheus Marciano de Oliveira <matheusmarciano294@gmail.com>
2023-09-20 19:37:59 -0300

    Controlador de rodadas

---
+++ Modulo_2/01_exercicios-backend-primeiro-servidor/01/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express ();
+
+const jogadores = ["José", "Maria", "João", "Marcos", "Fernanda"];
+
+let posicao = 0;
+
+app.get('/', function (req, res) {
+    res.send(`É a vez de ${jogadores[posicao]} jogar!`);
+    posicao++;
+
+    if (posicao >= (jogadores.length)){
+        posicao = 0;
+    };
+})
+
+app.listen(3000);
